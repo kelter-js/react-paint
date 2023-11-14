5ce8b600dcfa7e27c1d638b62584d8005651ca87
kelter-js <spawnkillerqw@gmail.com>
2023-11-14 20:50:51 +0500

    feat: updated store, preparations for modal components

--- src/shared/FilePanel.tsx
+++ src/shared/FilePanel.tsx
@@ -1,10 +1,14 @@
 import { saveAs } from "file-saver";
+import { useDispatch } from "react-redux";
 
 import { useCanvas } from "../CanvasContext";
 import { getCanvasImage } from "../utils/canvasUtils";
+import { show } from "../modules/modals/slice";
 
 const FilePanel = () => {
   const canvasRef = useCanvas();
+
+  const dispatch = useDispatch();
 
   const exportToFile = async () => {
     const file = await getCanvasImage(canvasRef.current);
@@ -16,6 +20,9 @@ const FilePanel = () => {
     saveAs(file, "drawing.png");
   };
 
+  const handleSave = () => dispatch(show("PROJECTS_SAVE_MODAL"));
+  const handleLoad = () => dispatch(show("PROJECTS_MODAL"));
+
   return (
     <div className="window file">
       <div className="title-bar">
@@ -26,6 +33,14 @@ const FilePanel = () => {
         <div className="field-row">
           <button className="save-button" onClick={exportToFile}>
             Export
+          </button>
+
+          <button className="save-button" onClick={handleSave}>
+            Save
+          </button>
+
+          <button className="save-button" onClick={handleLoad}>
+            Load
           </button>
         </div>
       </div>
--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import { logger } from "redux-logger";
 
 import { currentStroke } from "./modules/currentStroke/slice";
 import { modalVisible } from "./modules/modals/slice";
+import { RootState } from "./utils/types";
 import historyIndex from "./modules/historyIndex/slice";
 import strokes from "./modules/strokes/slice";
 
@@ -15,5 +16,7 @@ const store = configureStore({
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
+
+export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
 
 export default store;
