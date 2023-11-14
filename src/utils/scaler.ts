37a248da97d9b2456b1cad1ae4a7bd93c636b883
kelter-js <spawnkillerqw@gmail.com>
2023-11-14 20:51:09 +0500

    feat: implemented func to transform image in base64

---
+++ src/utils/scaler.ts
@@ -0,0 +1,43 @@
+type TScalerArgs = {
+  file: Blob;
+  scale: number;
+};
+
+const getBase64Thumbnail = ({
+  file,
+  scale = 0.1,
+}: TScalerArgs): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = (e) => {
+      const img = new Image();
+
+      img.onload = () => {
+        const canvasElement = document.createElement("canvas");
+        const ctx = canvasElement.getContext("2d");
+
+        if (!ctx) {
+          return;
+        }
+
+        const width = (canvasElement.width = img.width * scale);
+        const height = (canvasElement.height = img.height * scale);
+
+        ctx.drawImage(img, 0, 0, width, height);
+
+        return resolve(canvasElement.toDataURL());
+      };
+
+      img.src = e?.target?.result as string;
+    };
+
+    reader.onerror = (e) => {
+      reject(e.toString());
+    };
+  });
+};
+
+export default getBase64Thumbnail;
