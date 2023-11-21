61a11eed7c2b8467b2333c731fb5699f6f1f942c
kelter-js <spawnkillerqw@gmail.com>
2023-11-21 21:21:27 +0500

    feat: updated types for loading projects list

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -10,9 +10,20 @@ export type Stroke = {
   color: string;
 };
 
+export type Project = {
+  image: string;
+  name: string;
+  id: string;
+};
+
 export type RootState = {
   currentStroke: Stroke;
   strokes: Stroke[];
   historyIndex: number;
   modalVisible: ModalState;
+  projectsList: {
+    error?: string;
+    pending: boolean;
+    projects: Project[];
+  };
 };
